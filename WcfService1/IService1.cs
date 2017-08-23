a98149797de7f07d50f0fd0b6cfa6f717e67e764
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 17:46:02 +0700

    asda

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "users", ResponseFormat  = WebMessageFormat.Json)]
+        List<User> GetUserList();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "user/{id}", ResponseFormat = WebMessageFormat.Json)]
+        User GetUserById(string id);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "user/{email}/{pass}", ResponseFormat = WebMessageFormat.Json)]
+        bool IsValid(string email, string pass);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "user/add/{name}/{email}/{pass}", ResponseFormat = WebMessageFormat.Json)]
+        void AddUser(string name, string email, string pass);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "user/update", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        void UpdateUser(UserComposite usr);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "user/delete/{id}", ResponseFormat = WebMessageFormat.Json)]
+        void DeleteUser(string id);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class UserComposite
+    {
+        private int userID;
+        private string userName;
+        private string userEmail;
+        private string userPass;
+        string stringValue = "Hello ";
+
+        [DataMember(Name ="id")]
+        public int Id
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        [DataMember(Name ="name")]
+        public string Name
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        [DataMember(Name = "email")]
+        public string Email
+        {
+            get { return userEmail; }
+            set { userEmail = value; }
+        }
+        [DataMember(Name = "Password")]
+        public string Password
+        {
+            get { return userPass; }
+            set { userPass = value; }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<User> GetUserList()
+        {
+            using (UserEntities entities = new UserEntities())
+            {
+                return entities.User.ToList();
+            }
+        }
+        public User GetUserById(string id)
+        {
+            try
+            {
+                int UserId = Convert.ToInt32(id);
+                using (UserEntities entities = new UserEntities())
+                {
+                    return entities.User.SingleOrDefault(User => User.id == UserId);
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+        public bool IsValid(string em, string ps)
+        {
+            try
+            {
+                using (UserEntities entities = new UserEntities())
+                {
+                    var query = (from c in entities.User
+                                 where c.email == em && c.password == ps
+                                 select c).Any();
+                    return query;
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+        public void AddUser(string Name, string Email, string Password)
+        {
+            using (UserEntities entities = new UserEntities())
+            {
+                try
+                {
+                    User user = new User { name = Name, email = Email, password = Password };
+                    entities.User.Add(user);
+                    entities.SaveChanges();
+                }
+                catch
+                {
+                    throw new FaultException("Something went wrong");
+                }
+                
+            }
+        }
+        //take all class on function
+        public void UpdateUser(UserComposite usr)
+        {
+            try
+            {
+                int UserID = Convert.ToInt32(usr.Id);
+                using (UserEntities entities = new UserEntities())
+                {
+                    User wow = entities.User.SingleOrDefault(b => b.id == UserID);
+                    wow.name = usr.Name;
+                    wow.email = usr.Email;
+                    wow.password = usr.Password;
+                    entities.SaveChanges();
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+        public void DeleteUser(string id)
+        {
+            try
+            {
+                int UserID = Convert.ToInt32(id);
+                using (UserEntities entities = new UserEntities())
+                {
+                    User user = entities.User.SingleOrDefault(b => b.id == UserID);
+                    entities.User.Remove(user);
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+
+        
+    }
+}
